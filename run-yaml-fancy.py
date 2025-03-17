106798437be3df22d238a299494347d1a3042d7b
Dmitri Konstantinov <dkonst@mail.cern.ch>
2025-03-17 21:33:28 +0100

    Update run-yaml-fancy.py

--- run-yaml-fancy.py
+++ run-yaml-fancy.py
@@ -16,8 +16,8 @@ def generate_shields_url(test_name, geant4_version, compiler_version, stage, res
 
 # Function to generate Markdown table
 def generate_markdown_table(data):
-    table = "| Test Name | Geant4 Version | Compiler Version | Configure Result | Build Result |\n"
-    table += "|:----------|:--------------:|:----------------:|:----------------:|:------------:|\n"
+    table = "| Test Name | Geant4 Version | Compiler Version | Configure Result | Build Result | Run Result |\n"
+    table += "|:----------|:--------------:|:----------------:|:----------------:|:------------:|:------------:|\n"
 
     last_test_name = ""
     for test in sorted(data['tests'], key=lambda x: x['name']):
@@ -25,10 +25,12 @@ def generate_markdown_table(data):
             for compiler in sorted(version['compilers'], key=lambda x: x['compiler_version']):
                 configure_result = compiler['results']['configure']
                 build_result = compiler['results']['build']
+                run_result = compiler['results']['run']
                 configure_badge = f"![{configure_result}]({generate_shields_url(test['name'], version['version'], compiler['compiler_version'], 'configure', configure_result)})"
                 build_badge = f"![{build_result}]({generate_shields_url(test['name'], version['version'], compiler['compiler_version'], 'build', build_result)})"
+                run_badge = f"![{run_result}]({generate_shields_url(test['name'], version['version'], compiler['compiler_version'], 'run', run_result)})"
                 test_name_display = test['name'] if test['name'] != last_test_name else ""
-                row = f"| {test_name_display} | {version['version']} | gcc{compiler['compiler_version']} | {configure_badge} | {build_badge} |\n"
+                row = f"| {test_name_display} | {version['version']} | gcc{compiler['compiler_version']} | {configure_badge} | {build_badge} | {run_badge} |\n"
                 table += row
                 last_test_name = test['name']
     return table
